97e9e25ebac603c87ed0e101288996367e40d5fd
U-DESKTOP-A9A3B54\jakedavies <jgdavies24@gmail.com>
2016-03-16 23:26:12 -0700

    vector calculations implemented

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -2,9 +2,20 @@ const redis = require('redis');
 const pmongo = require('promised-mongo');
 
 
-const db = pmongo('192.168.99.100/db', ['device_sessions']);
+const db = pmongo('192.168.99.100/db', ['device_sessions', 'maps']);
 const walkingSpeed = 1.38582;
 const degreesPerRadian = 0.0174533;
+
+const timeHeadingVector = function timeHeadingVector(heading, elapsedTime) {
+  const headingInRadians = heading / degreesPerRadian;
+  const distanceScaler = walkingSpeed * elapsedTime;
+  console.log(headingInRadians);
+  const vector = [
+    Math.cos(headingInRadians) * distanceScaler,
+    Math.sin(headingInRadians) * distanceScaler,
+  ];
+  return vector;
+};
 
 exports.render = (req, res) => {
   const heading = req.body.heading;
@@ -18,29 +29,45 @@ exports.render = (req, res) => {
     ]);
   })
   .then(result => {
-    const map = result[0];
+    const map = result[0][0];
     const deviceEntries = result[1];
-    if (deviceEntries.beacons.length > 1) {
-      const thisDeviceEntry = deviceEntries[0];
-      const beaconInRange = thisDeviceEntry.beacons[0];
-      const beacon = map.beacons.filter(thisBeacon => thisBeacon.major === beaconInRange.major)[0];
-      const elapsedTime = (Date.now() - beaconInRange.date) / 1000;
-      const distanceScaler = walkingSpeed * elapsedTime;
-      const headingInRadians = thisDeviceEntry.heading * degreesPerRadian;
-      const vector = [
-        Math.cos(headingInRadians) / distanceScaler,
-        Math.sin(headingInRadians) / distanceScaler,
-      ];
-      res.json({ position: { x: beacon.x + vector[0], y: beacon.y + vector[1] } });
-    } else if (deviceEntries.beacons.length === 1) {
-      
+    const thisDeviceEntry = deviceEntries[0];
+    const beaconInRange = thisDeviceEntry.beacons[0];
+    const beacon = map.beacons.filter(thisBeacon => thisBeacon.major === beaconInRange.major)[0];
+    const elapsedTime = (Date.now() - thisDeviceEntry.date) / 1000;
+    const vector = timeHeadingVector(thisDeviceEntry.heading, elapsedTime);
+    if (deviceEntries.length > 1) {
+      let lastDevice = deviceEntries[0];
+      const vectorsInOrderOfImportance = [vector];
+      for (let i = 1; i < deviceEntries.length; i++) {
+        // if we are now at another beacon we need to factor
+        //  the vector from beacon a to beacon b
+        // at the highest we can
+        if (lastDevice.beacons[0].major !== deviceEntries[i].beacons.major) {
+          const trueBeaconVector = {
+            x: deviceEntries[i].beacons[0].x - lastDevice.beacons[0].x,
+            y: deviceEntries[i].beacons[0].y - lastDevice.beacons[0].y,
+          };
+          const headingTimeDelta = timeHeadingVector(lastDevice.header, deviceEntries[i].date - lastDevice.date);
+          // fuse these two vectors to determine something ?
+          vector.push(headingTimeDelta);
+        }
+        // update last device
+        lastDevice = deviceEntries[i];
+      }
+      // from the series of previous vectors, determine our current position?
+      return res.json({ err: 'Not yet implemented' });
+    } else if (deviceEntries.length === 1) {
+      return res.json({ position: { x: beacon.x + vector[0], y: beacon.y + vector[1] } });
     }
     return res.json({ err: 'No beacons found in range' });
   })
-  .then((result) => {
-    res.json({dick: 'test'});
-  })
   .catch((err) => {
-    res.json({err: err});
+    console.log(err);
+    res.json({ err: err });
   });
 };
+
+
+
+
