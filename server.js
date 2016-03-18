c9a8d900d2bf9c4f31720d8acd8f118264c71911
jakedavies <jgdavies24@gmail.com>
2016-03-17 19:58:18 -0700

    bind to running port better

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const express = require('./config/express');
 
-var express = require( './config/express' );
+const app = express();
+app.set('port', (process.env.PORT || 5000));
 
-var app = express();
-const port = process.env.PORT ? process.env.PORT : 3030;
-app.listen( port, '127.0.0.1' );
-
-console.log( 'temp app running at http://localhost:'+process.env.PORT+'/' );
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
+});
