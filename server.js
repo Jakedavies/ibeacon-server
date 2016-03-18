be3f0467bd5da31f3b6e2949b0fa402c9e317ed0
jakedavies <jgdavies24@gmail.com>
2016-03-17 19:39:54 -0700

    crushing it

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require( './config/express' );
 
 var app = express();
-const port = process.env.PORT ? process.env.port : 3030;
+const port = process.env.PORT ? process.env.PORT : 3030;
 app.listen( port, '127.0.0.1' );
 
-console.log( 'temp app running at http://localhost:3030/' );
+console.log( 'temp app running at http://localhost:'+process.env.PORT+'/' );
